748811e6679e1c55699eadb2763f6206d3cbc1a7
luihum <luistoo.favaro@gmail.com>
2021-12-02 13:01:42 -0300

    Finally made it kinda work, you need ADB to really use it tho gonna add info to le readme

--- app/src/main/java/com/luihum/progressarchiver95/ApplicationMain.kt
+++ app/src/main/java/com/luihum/progressarchiver95/ApplicationMain.kt
@@ -1,9 +1,4 @@
 package com.luihum.progressarchiver95
 
 import android.app.Application
-import android.content.Context
-
-class ApplicationMain: Application() {
-    val appContext: Context = this.applicationContext
-    val archiveBaseDir = appContext.getDir(appContext.filesDir.absolutePath + "archive", MODE_PRIVATE)
-    }
+class ApplicationMain: Application()
